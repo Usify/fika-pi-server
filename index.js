675ae6069b109e911ae16adbf66db30b84e2944b
Benny Samuelsson <benny.samuelsson@usify.se>
2017-05-15 14:15:18 +0200

    fixed indentation

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express'); // call express
+var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 var http = require('http');
 var querystring = require('querystring');
@@ -13,22 +13,22 @@ var querystring = require('querystring');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/on/:state', function(extreq, res) {
-    res.json({ message: 'hooray! welcome to our api!' });  
+    res.json({ message: 'hooray! welcome to our api!' });
 
-	console.log("EXT REQUEST BODY STATE PARAM: " + extreq.params.state);
+    console.log("EXT REQUEST BODY STATE PARAM: " + extreq.params.state);
 
-	let stateParam = (extreq.params.state == "true");
+    let stateParam = (extreq.params.state == "true");
 
-    const postData = JSON.stringify({"on": stateParam});
-	console.log(postData);
+    const postData = JSON.stringify({ "on": stateParam });
+    console.log(postData);
     var options = {
         host: '192.168.5.132',
         port: 80,
@@ -40,17 +40,17 @@ router.get('/on/:state', function(extreq, res) {
         }
     };
 
-       var req = http.request(options, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                console.log('LAMP REQUEST BODY: ' + chunk);
-            });
+    var req = http.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            console.log('LAMP REQUEST BODY: ' + chunk);
         });
+    });
 
-        req.write(postData);
-        req.end();
+    req.write(postData);
+    req.end();
 
 });
 
