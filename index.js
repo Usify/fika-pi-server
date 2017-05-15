9852d82a831a9320457fa67958d5929f8393977e
LKPG DEVS <rnd@usify.se>
2017-05-15 12:13:25 +0000

    fixed on state route

--- index.js
+++ index.js
@@ -20,18 +20,22 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
+router.get('/on/:state', function(extreq, res) {
     res.json({ message: 'hooray! welcome to our api!' });  
 
-    const postData = querystring.stringify({"on":"true"});
+	console.log("EXT REQUEST BODY STATE PARAM: " + extreq.params.state);
 
+	let stateParam = (extreq.params.state == "true");
+
+    const postData = JSON.stringify({"on": stateParam});
+	console.log(postData);
     var options = {
         host: '192.168.5.132',
         port: 80,
         path: '/api/tyObM0Dd4TlMYGoqua7rFBcPNPQBVkJ5eZt8lfrM/lights/1/state',
         method: 'PUT',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/json',
             'Content-Length': Buffer.byteLength(postData)
         }
     };
@@ -41,7 +45,7 @@ router.get('/', function(req, res) {
             console.log('HEADERS: ' + JSON.stringify(res.headers));
             res.setEncoding('utf8');
             res.on('data', function (chunk) {
-                console.log('BODY: ' + chunk);
+                console.log('LAMP REQUEST BODY: ' + chunk);
             });
         });
 
