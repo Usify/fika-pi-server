d735a49c5ba9857cafd59e2cdabf846d071c2a52
Tina Blunt <tinablunt@gmail.com>
2017-05-15 13:26:55 +0200

    Tänd lampan PUT

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var querystring = require('querystring');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -19,7 +20,33 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });  
+
+    const postData = querystring.stringify({"on":true});
+
+    var options = {
+        host: 'http://192.168.5.132',
+        port: 80,
+        path: '/api/tyObM0Dd4TlMYGoqua7rFBcPNPQBVkJ5eZt8lfrM/lights/1/state',
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+       var req = http.request(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+            });
+        });
+
+        req.write(postData);
+        req.end();
+
 });
 
 // more routes for our API will happen here
