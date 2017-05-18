79a73c2469396435df2065513d2aabf716109564
Benny Samuelsson <benny.samuelsson@usify.se>
2017-05-18 11:27:24 +0200

    commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 var http = require('http');
 var querystring = require('querystring');
+var socketio = require("socketio");
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -14,6 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080; // set our port
+
+// SOCKET LISTENER FOR OUR API
+var socket = socketio("http://fika-api-dev.eu-central-1.elasticbeanstalk.com/");
+socket.on('message', function(data) {
+    //$('#messages').append($('<li>').text(JSON.stringify(data.message)));
+    console.log(data);
+});
 
 // ROUTES FOR OUR API
 // =============================================================================
